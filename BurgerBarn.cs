c00329d8176584f534ab737921d73366448e1448
CTH-npitx <chatch@npitx.org>
2024-09-03 15:14:44 -0500

    nonsecsusful

---
+++ BurgerBarn.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BurgerBarn
+{
+    public partial class BurgerBarn : Form
+    {
+        public BurgerBarn()
+        {
+            InitializeComponent();
+        }
+
+        private void BurgerBarn_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
