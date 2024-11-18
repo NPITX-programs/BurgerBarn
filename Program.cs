ac66e09484aef6950f13f9f7ebcedf72eef268dd
CTH-npitx <chatch@npitx.org>
2024-11-18 14:16:07 -0600

    alter name of .cs to be the original one

--- Program.cs
+++ Program.cs
@@ -3,7 +3,7 @@ using System.Windows.Forms;
 
 namespace BurgerBarn
 {
-    internal static class StartupCode
+    internal static class Program
     {
         /// <summary>
         /// The main entry point for the application.
