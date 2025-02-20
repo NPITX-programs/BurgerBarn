40df183c072c57dc195ea39338567f343fea045a
Um... Figure it out! <33612115+CTH999@users.noreply.github.com>
2025-02-20 15:54:22 -0600

    discount
    modified such that the discount now exists

--- BurgerBarn.cs
+++ BurgerBarn.cs
@@ -7,8 +7,8 @@ namespace BurgerBarn
     {
         float subtotal = 0;
         const float tax_rate = 0.0825f;
-        float discount = 0; //the default discount of 0%
-        const float discValue = 20f; //the discount, as a percent. The code subtracts it from 100, then divides by 100. So, putting in 20 is the same as 20% off, which becomes .8
+        float discount = 1; //no discount, aka 100% (a.k.a. 1 times) the normal price
+        const float discValue = 20f; //the discount, as a percent. The code subtracts it from 100, then divides by 100. So, putting in 20 is the same as 20% off, which becomes .8 (what that means
         
 
         public BurgerBarn()
@@ -36,9 +36,9 @@ namespace BurgerBarn
                 lstOrder.Items.Remove(rdb.Text); //removes Items from order
                 subtotal -= float.Parse(rdb.Tag.ToString()); // removes price from receipt
             }
-            //calculate totals
-            float multVal = (100 - discount) / 100;
+            //calculate totalss
             float tax = subtotal * tax_rate;
+            subtotal = subtotal * discount; //applies the discount. By doing it after the tax, tax is based on the original subtotal. By doing it before total, the total factors in the discount, meaning it's the discounted subtotal + the non-discounted tax. if you want the discount to include tax, just move this one line up
             float total = subtotal + tax;
 
             //display totals
@@ -66,7 +66,7 @@ namespace BurgerBarn
         private void bttnCoupon_Click(object sender, EventArgs e)
         {
             bttnCoupon.Enabled = false; //disable the coupon button once applied
-            discount = discValue; //sets the discount to be the discount value
+            discount = (100 - discValue) / 100; //sets the discount to be be the reciprical of the discount value as a percent, so that the value to pay can be calculated by multiplication. So, a 20% discount (an input of 20) is the same as 80% of the original price. The 100-"x" turns 20 to 80. Then, divide by 100, and you get 0.8, or 80 percent.
         }
     }
 }
