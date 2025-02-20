52dbf9221efb58a442363e1ef39c0edfabc34944
Um... Figure it out! <33612115+CTH999@users.noreply.github.com>
2025-02-20 14:44:32 -0600

    add apply coupon button

--- BurgerBarn.Designer.cs
+++ BurgerBarn.Designer.cs
@@ -61,6 +61,8 @@
             this.rdbChicSand = new System.Windows.Forms.RadioButton();
             this.rdbItalSub = new System.Windows.Forms.RadioButton();
             this.rdbTurkClub = new System.Windows.Forms.RadioButton();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
             this.groupBox1.SuspendLayout();
             this.groupBox2.SuspendLayout();
             this.groupBox3.SuspendLayout();
@@ -502,11 +504,31 @@
             this.rdbTurkClub.UseVisualStyleBackColor = true;
             this.rdbTurkClub.CheckedChanged += new System.EventHandler(this.rdb_selectItem);
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(0, 0);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 11;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(340, 438);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 31);
+            this.button2.TabIndex = 12;
+            this.button2.Text = "Apply Coupon";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
             // BurgerBarn
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 518);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.gbx_sandwhiches);
             this.Controls.Add(this.btnExit);
             this.Controls.Add(this.btnPurchase);
@@ -570,6 +592,8 @@
         private System.Windows.Forms.RadioButton rdbChicSand;
         private System.Windows.Forms.RadioButton rdbItalSub;
         private System.Windows.Forms.RadioButton rdbTurkClub;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
     }
 }
 
